9b97bc7b9b521d34f1a2bde3220c44a0f366822d
Cloudhax23 <alexgrantnrp@gmail.com>
2017-09-15 22:21:35 -0500

    Update EventHandler.cpp

--- EventHandler.cpp
+++ EventHandler.cpp
@@ -8,3 +8,14 @@ void EventHandler::AddEventHandler(char* name, void* func)
 {
 	EventsMap.insert(std::pair<char*, void*>(name, func));
 }
+
+template<typename T, typename ... Args >
+void EventHandler::TriggerEventHandler(char* name, Args ... args)
+{
+	std::pair<std::multimap<char*, void*>::iterator, std::multimap<char*, void*>::iterator> mapIterator;
+	mapIterator = EventHandler::EventsMap.equal_range(name);
+	for (std::multimap<char*, void*>::iterator eventIterator = mapIterator.first; eventIterator != mapIterator.second; ++eventIterator)
+	{
+		((T*)((*eventIterator).second)) (args...);
+	}
+};
