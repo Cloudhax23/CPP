b217566a6afb7e0676c6ab946a0e96439e387b25
Cloudhax23 <alexgrantnrp@gmail.com>
2017-09-15 22:21:53 -0500

    Update EventHandler.h

--- EventHandler.h
+++ EventHandler.h
@@ -7,15 +7,6 @@ class EventHandler
 public:
 	static std::multimap<char*, void*> EventsMap;
 	static void AddEventHandler(char*, void*);
+	template<typename T, typename ... Args >
+	static void TriggerEventHandler(char* name, Args ... args);
 };
-
-template<typename T, typename ... Args >
-static void TriggerEventHandler(char* name, Args ... args)
-{
-	std::pair<std::multimap<char*, void*>::iterator, std::multimap<char*, void*>::iterator> mapIterator;
-	mapIterator = EventHandler::EventsMap.equal_range(name);
-	for (std::multimap<char*, void*>::iterator eventIterator = mapIterator.first; eventIterator != mapIterator.second; ++eventIterator)
-	{
-		((T*)((*eventIterator).second)) (args...);
-	}
-}
